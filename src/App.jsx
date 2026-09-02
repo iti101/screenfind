2da47ebd9d0ed674243d090e41a17bce706a1113
itamar <itamar.turgman@gmail.com>
2026-09-02 19:55:33 +0200

    Add Auth Context, app shell layout, and React Router routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-import LandingPage from './pages/landingPage.jsx'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import ContactPage from './pages/ContactPage.jsx'
+import DetailPage from './pages/DetailPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
+import SearchPage from './pages/SearchPage.jsx'
+import WatchlistPage from './pages/WatchlistPage.jsx'
 import './App.css'
 
 function App() {
-  return <LandingPage />
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="details/:mediaType/:id" element={<DetailPage />} />
+        <Route
+          path="watchlist"
+          element={
+            <PrivateRoute>
+              <WatchlistPage />
+            </PrivateRoute>
+          }
+        />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
